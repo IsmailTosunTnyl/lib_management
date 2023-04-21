6d1edc5902c3de410316ff2880c5a5b5a927d343
itosun <ismailtosuntnyl@gmail.com>
2023-04-21 23:32:36 +0300

    init

---
+++ lib/main.dart
@@ -0,0 +1,167 @@
+import 'package:flutter/material.dart';
+// import firebase_core plugin
+import 'package:firebase_core/firebase_core.dart';
+// import cloud_firestore plugin
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+import 'firebase_options.dart';
+import 'package:lib_management/widgets/book.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
+  runApp(const MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  // This widget is the root of your application.
+
+  final Future<FirebaseApp> _initialization = Firebase.initializeApp();
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
+        primarySwatch: Colors.blue,
+      ),
+      home: FutureBuilder(
+        // Initialize FlutterFire:
+        future: _initialization,
+        builder: (context, snapshot) {
+          // Check for errors
+          if (snapshot.hasError) {
+            return const Center(
+                child: Text('Something went wrong Check Connection'));
+          }
+
+          // Once complete, show your application
+          if (snapshot.connectionState == ConnectionState.done) {
+            return const MyHomePage(title: 'Flutter Demo Home Page');
+          }
+
+          // Otherwise, show something whilst waiting for initialization to complete
+          return const Center(child: CircularProgressIndicator());
+        },
+      ),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+  final Stream<QuerySnapshot> _testStream =
+      FirebaseFirestore.instance.collection('Test').snapshots();
+
+  final Stream<QuerySnapshot> _booksStream =
+      FirebaseFirestore.instance.collection('Books').snapshots();
+
+  void _incrementCounter() {
+    setState(() {
+      FirebaseFirestore.instance.collection('Books').doc("1984").set({
+        "author": "George Orwell",
+        "available": 3,
+        "image":
+            "https://i.dr.com.tr/cache/500x400-0/originals/0000000064038-1.jpg",
+        "name": "1984",
+        "publisher": "Can Yayınları"
+      });
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: StreamBuilder<QuerySnapshot>(
+          stream: _booksStream,
+          builder:
+              (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+            if (snapshot.hasError) {
+              return const Text('Something went wrong');
+            }
+
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return const Text("Loading");
+            }
+
+            return Column(
+              children: [
+                SizedBox(
+                  height: 600,
+                  child: GridView(
+                    gridDelegate:
+                        const SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 2,
+                      childAspectRatio: 0.7,
+                    ),
+                    children:
+                        snapshot.data!.docs.map((DocumentSnapshot document) {
+                      final Map<String, dynamic> data =
+                          document.data()! as Map<String, dynamic>;
+
+                      return BookWidget(
+                        book: Book(
+                            title: data['name'],
+                            author: data['author'],
+                            publisher: data['publisher'],
+                            booksAvailable: data['available'],
+                            image: data['image']),
+                      );
+                    }).toList(),
+                  ),
+                ),
+              ],
+            );
+          },
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
---
+++ lib/widgets/book.dart
@@ -0,0 +1,277 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+
+class Book {
+  final String title;
+  final String author;
+  final String publisher;
+  final int booksAvailable;
+  final String image;
+
+  Book(
+      {required this.title,
+      required this.author,
+      required this.publisher,
+      required this.booksAvailable,
+      required this.image});
+}
+
+class BookWidget extends StatefulWidget {
+  final Book book;
+  const BookWidget({super.key, required this.book});
+
+  @override
+  State<BookWidget> createState() => _BookWidgetState();
+}
+
+// open dialog for book details
+void _openBookDetails(BuildContext context, Book book) {
+  int deviceWidth = MediaQuery.of(context).size.width.toInt();
+  int deviceHeight = MediaQuery.of(context).size.height.toInt();
+  showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return Dialog(
+            insetAnimationCurve: Curves.bounceInOut,
+            elevation: 22,
+            child: Container(
+              decoration: const BoxDecoration(
+                  gradient: LinearGradient(
+                begin: Alignment.topLeft,
+                end: Alignment.bottomRight,
+                colors: [
+                  Color.fromARGB(255, 255, 255, 255),
+                  Color.fromARGB(124, 176, 177, 173)
+                ],
+              )),
+              padding: deviceHeight > 800
+                  ? const EdgeInsets.all(20)
+                  : const EdgeInsets.fromLTRB(10, 10, 10, 20),
+              width: deviceWidth > 800 ? deviceWidth * 0.3 : deviceWidth * 0.8,
+              height: deviceHeight * 0.6,
+              child: Column(
+                children: [
+                  Expanded(
+                    flex: 27,
+                    child: Container(
+                      margin: const EdgeInsets.all(10),
+                      color: const Color.fromARGB(62, 120, 33, 141),
+                      child: Image.network(book.image, fit: BoxFit.fitWidth,
+                          errorBuilder: (BuildContext context, Object exception,
+                              StackTrace? stackTrace) {
+                        print(exception);
+                        return const Icon(Icons.book, size: 200);
+                      }),
+                    ),
+                  ),
+                  Expanded(
+                    flex: 3,
+                    child: Container(
+                      //color: Colors.green,
+                      child: Text(
+                        book.title,
+                        textAlign: TextAlign.center,
+                        style: const TextStyle(
+                            fontSize: 20, fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                  ),
+                  Expanded(
+                    flex: 3,
+                    child: Container(
+                      //color: Colors.yellow,
+                      child: Text(
+                        book.author,
+                        textAlign: TextAlign.center,
+                        style: const TextStyle(
+                            fontSize: 15, fontWeight: FontWeight.w400),
+                      ),
+                    ),
+                  ),
+                  Expanded(
+                    flex: 3,
+                    child: Container(
+                      //color: Colors.yellow,
+                      child: Text(
+                        book.publisher,
+                        textAlign: TextAlign.center,
+                        style: const TextStyle(
+                            fontSize: 15, fontWeight: FontWeight.w200),
+                      ),
+                    ),
+                  ),
+                  Expanded(
+                    flex: 5,
+                    child: Container(
+
+                        //color: Colors.yellow,
+                        child: book.booksAvailable > 0
+                            ? ElevatedButton(
+                                style: ElevatedButton.styleFrom(
+                                  elevation: 10,
+                                  primary:
+                                      const Color.fromARGB(255, 155, 215, 33),
+                                  onPrimary: Colors.white,
+                                  shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(15),
+                                  ),
+                                ),
+                                onPressed: () {
+                                  // Get Book TODO
+                                  print(book.title);
+                                  Navigator.pop(context);
+                                },
+                                child: const Text(
+                                  "Get Book",
+                                  style: TextStyle(
+                                      color: Colors.white, fontSize: 20),
+                                ))
+                            : const Text(
+                                textAlign: TextAlign.center,
+                                "No Books Available",
+                                style: TextStyle(
+                                    color: Colors.red,
+                                    fontSize: 20,
+                                    fontWeight: FontWeight.bold),
+                              )),
+                  ),
+                  Expanded(
+                    flex: 3,
+                    child: Align(
+                      alignment: Alignment.bottomRight,
+                      child: TextButton(
+                        onPressed: () {
+                          Navigator.pop(context);
+                        },
+                        child: const Text(
+                          "Close",
+                          style: TextStyle(color: Colors.blue, fontSize: 15),
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ));
+      });
+}
+
+class _BookWidgetState extends State<BookWidget> {
+  @override
+  Widget build(BuildContext context) {
+    int deviceWidth = MediaQuery.of(context).size.width.toInt();
+
+    return InkWell(
+      onTap: () => _openBookDetails(context, widget.book),
+      child: Card(
+          // border on card black
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(15.0),
+          ),
+          elevation: 10,
+          child: Container(
+              padding: const EdgeInsets.all(8),
+              margin: const EdgeInsets.all(10),
+              height: deviceWidth * 0.63,
+              width: 50,
+              //change color of container
+              //color: Colors.red,
+              child: Column(
+                children: [
+                  //image
+                  Expanded(
+                    flex: 27,
+                    child: Container(
+                      height: deviceWidth * 0.63 * 0.6,
+                      width: double.infinity,
+                      color: const Color.fromARGB(62, 120, 33, 141),
+                      child: Image.network(widget.book.image,
+                          fit: BoxFit.fitHeight, errorBuilder:
+                              (BuildContext context, Object exception,
+                                  StackTrace? stackTrace) {
+                        print(exception);
+                        return const Icon(Icons.book, size: 200);
+                      }),
+                    ),
+                  ),
+                  //title
+                  Expanded(
+                    flex: 4,
+                    child: Container(
+                      width: double.infinity,
+                      //color: Colors.green,
+                      child: Text(
+                        widget.book.title,
+                        textAlign: TextAlign.center,
+                        style: const TextStyle(
+                            fontSize: 20, fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 5,
+                  ),
+                  //author
+                  Expanded(
+                    flex: 3,
+                    child: Container(
+                      width: double.infinity,
+                      //color: Colors.yellow,
+                      child: Text(
+                        widget.book.author,
+                        textAlign: TextAlign.center,
+                        style: const TextStyle(
+                            fontSize: 15, fontWeight: FontWeight.w400),
+                      ),
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 5,
+                  ),
+                  // books available
+                  Expanded(
+                    flex: 3,
+                    child: Container(
+                        width: double.infinity,
+                        //color: Colors.orange,
+                        child: widget.book.booksAvailable > 0
+                            ? Text(
+                                '${widget.book.booksAvailable} books available',
+                                textAlign: TextAlign.center,
+                                style: const TextStyle(
+                                    fontSize: 13,
+                                    fontWeight: FontWeight.w200,
+                                    color: Colors.green),
+                              )
+                            : const Text(
+                                'No books available',
+                                textAlign: TextAlign.center,
+                                style: TextStyle(
+                                    fontSize: 13,
+                                    fontWeight: FontWeight.w200,
+                                    color: Colors.red),
+                              )),
+                  ),
+                  const SizedBox(
+                    height: 5,
+                  ),
+                  // book seller
+                  Expanded(
+                    flex: 2,
+                    child: Container(
+                      width: double.infinity,
+                      //color: Colors.purple,
+                      child: Text(
+                        widget.book.publisher,
+                        textAlign: TextAlign.center,
+                        style: const TextStyle(
+                            fontSize: 12, fontWeight: FontWeight.w100),
+                      ),
+                    ),
+                  ),
+                ],
+              ))),
+    );
+  }
+}
